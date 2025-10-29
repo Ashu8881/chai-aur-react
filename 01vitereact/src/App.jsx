e7833f19760bea3329944f9ac464882d3adf6aa0
Ashutosh <115519946+Ashu8881@users.noreply.github.com>
2025-10-29 23:54:37 +0530

    get jsx how to render jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
-
-
 function App() {
-  
-
+  const username = "Ashutosh"
   return (
- <><Chai /><Test01 /></>
-  )
+    <>
+      <Chai />
+      <Test01 />
+      <h1>My Name {username}</h1>
+    </>
+  );
 }
-import Chai from "./chai"
-import Test01 from "./test"
-export default App
+import Chai from "./chai";
+import Test01 from "./test";
+export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
+// import {jsx as _jsx} from "react/jsx-runtime.js";
+import App from "./App.jsx";
 
-import App from './App.jsx'
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>testing react 121</h1>
+//     </div>
+//   );
+// }
+// const anotherElement = (
+//      <a href="https:// google.com" target="_blank">visit google</a>
+// )
 
-createRoot(document.getElementById('root')).render(
-     <App />
-,
-)
+// const reactElement = React.createElement('a', {hrer:'https://google.com' ,target :"_blank",},"click me to visit")
+createRoot(document.getElementById("root"))
+  .render(
+
+  // reactElement
+  // <div>
+  // <MyApp/>
+  <App />
+  //</div>
+  // anotherElement
+  );
